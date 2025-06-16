e97719e13df16cd98c12b0f2718ff7b6d4aff0e1
SamedovResul <samedovrasul7@gmail.com>
2025-06-16 15:38:29 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+
+import './index.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [items, setItems] = useState([]);
+  const [name, setName] = useState('');
+
+  useEffect(() => {
+    fetchItems();
+  }, []);
+
+  const fetchItems = async () => {
+    const res = await axios.get('/api/items');
+    setItems(res.data);
+  };
+
+  const addItem = async () => {
+    await axios.post('/api/items', { name });
+    setName('');
+    fetchItems();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<div className="app-container">
+      <h1>MERN DevOps Demo</h1>
+      <div className="form">
+        <input
+          type="text"
+          placeholder="Enter item"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <button onClick={addItem}>Add Item</button>
+      </div>
+      <ul className="item-list">
+        {items.map((item) => (
+          <li key={item._id}>{item.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
 
 export default App;
+
